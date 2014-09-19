fda9059d8357e5c8dc60893678a35ea1d55dc240
Guido Grassel <guido@Guidos-MacBook-Pro.local>
2014-09-19 17:57:45 +0300

    fixed a bug related to binding Model class to the Entity definition

--- myList/Model.swift
+++ myList/Model.swift
@@ -14,4 +14,10 @@ class Model: NSManagedObject {
     @NSManaged var quantity : String;
     @NSManaged var info : String;
     
+    /* see https://developer.apple.com/library/prerelease/mac/documentation/Swift/Conceptual/BuildingCocoaApps/WritingSwiftClassesWithObjective-CBehavior.html
+    section : Implementing Core Data Managed Object Subclasses
+    
+    Swift classes are namespaced—they’re scoped to the module (typically, the project) they are compiled in. To use a Swift subclass of the NSManagedObject class with your Core Data model, prefix the class name in the Class field in the model entity inspector with the name of your module.
+    */
+    
 }
